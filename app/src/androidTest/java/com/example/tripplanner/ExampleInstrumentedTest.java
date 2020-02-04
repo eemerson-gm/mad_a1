a8563f0081f5dab7dda0b97bb783815ae8c9f379
Eric Emerson <EricEmerson@users.noreply.github.com>
2020-02-03 19:17:09 -0500

    Added starting page.

--- app/src/androidTest/java/com/example/tripplanner/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/tripplanner/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.example.tripplanner;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
